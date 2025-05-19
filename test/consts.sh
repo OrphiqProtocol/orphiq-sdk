55ba62e5170f02dd815f57883847e3fcb4ecbf50
Orphiq <Orphiqprotocol@outlook.com>
2025-05-19 17:54:41 -0400

    Add files via upload

---
+++ test/consts.sh
@@ -0,0 +1,18 @@
+#
+# oasis-core version selection.
+#
+# We'll download from the first applicable source that's non-empty.
+#
+
+# Released version from GitHub Releases.
+OASIS_CORE_VERSION='25.2'
+
+# Development version from GitHub Actions.
+# e.g. '58512799'
+GITHUB_ARTIFACT='' # 5214f87
+# e.g. '21.1-dev'
+GITHUB_ARTIFACT_VERSION=''
+
+# Version from Buildkite.
+# e.g. '4759'
+BUILD_NUMBER='15204'
